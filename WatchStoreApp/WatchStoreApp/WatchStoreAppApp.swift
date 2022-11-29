bbf40bdcf47d9d3854190ab5ae57799d022f1963
Alandis <1seals.alandis@gmail.com>
2022-11-29 10:49:02 -0600

    initial commit

---
+++ WatchStoreApp/WatchStoreApp/WatchStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchStoreAppApp.swift
+//  WatchStoreApp
+//
+//  Created by Alandis Seals on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
