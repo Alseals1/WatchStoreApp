3debe2c843c41e91ae4db922d4f5e73611e787e1
Alandis <1seals.alandis@gmail.com>
2023-10-16 09:53:01 -0500

    Initial Commit

---
+++ AppleWatchStore/AppleWatchStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchStoreApp.swift
+//  AppleWatchStore
+//
+//  Created by Alandis Seals on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
